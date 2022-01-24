728b86a5d448936bede560fac5c857a3de4259a7
Alexis Mastrodomenico <86946841+LexMastro@users.noreply.github.com>
2022-01-24 21:54:09 +1100

    added carousel

--- client/src/PortfolioContainer/Projects/Projects.js
+++ client/src/PortfolioContainer/Projects/Projects.js
@@ -1,8 +1,6 @@
 import React from "react";
-// import Link  from "react-router-dom";
-import OwlCarousel from "react-owl-carousel";
-import "owl.carousel/dist/assets/owl.carousel.css";
-import "owl.carousel/dist/assets/owl.theme.default.css";
+import { useState } from "react";
+import { Carousel } from "react-bootstrap";
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import ScrollService from "../../utilities/ScrollService";
 import Animations from "../../utilities/Animations";
@@ -12,93 +10,53 @@ import weather from "../../../src/img/Projects/weather.png";
 import lettuceEat from "../../../src/img/Projects/LettuceEat.png";
 import noteTaker from "../../../src/img/Projects/notetaker.jpg";
 
-export default function project(props) {
+// function clickme(event) {
+//   console.log(event.target, "aaa");
+// }
+
+export default function Project(props) {
   let fadeInScreenHandler = (screen) => {
     if (screen.fadeInScreen !== props.id) return;
     Animations.animations.fadeInScreen(props.id);
   };
-
   const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+  const [index, setIndex] = useState(0);
 
-  const options = {
-    loop: true,
-    margin: 0,
-    nav: true,
-    animateIn: "bounceInRight",
-    animateOut: "bounceOutRight",
-    dots: true,
-    autoplay: true,
-    smartSpeed: 900,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      768: {
-        items: 1,
-      },
-      1000: {
-        items: 3,
-      },
-    },
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
   };
 
   return (
-    <div>
-      <ScreenHeading title={"Projects"} />
-      <section className="project-section fade-in" id={props.id || ""}>
-        <div className="container">
-          <div className="row">
-            <OwlCarousel
-              className="owl-carousel"
-              id="project-carousel"
-              {...options}
-            >
-              <div className="col-lg-12">
-                <div className="project-item">
-                  <div className="project-info">
-                    <img src={beautyboard} alt="no internet connection"></img>
-                  </div>
-                </div>
-                <div className="project-name">
-                  <p>Beauty Board</p>
-                </div>
-              </div>
+    <div className="carousel screen-container fade-in" id={props.id || ""}>
+      <ScreenHeading title={"Projects"} />;
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item className="carousel-item">
+          <img className="d-block" src={beautyboard} alt="First slide" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block" src={lettuceEat} alt="Second slide" />
 
-              <div className="col-lg-12">
-                <div className="project-item">
-                  <div className="project-info">
-                    <img src={weather} alt="no internet connection"></img>
-                  </div>
-                </div>
-                <div className="project-name">
-                  <p>Weather Dashboard</p>
-                </div>
-              </div>
-              <div className="col-lg-12">
-                <div className="project-item">
-                  <div className="project-info">
-                    <img src={noteTaker} alt="no internet connection"></img>
-                  </div>
-                </div>
-                <div className="project-name">
-                  <p>Note Taker</p>
-                </div>
-              </div>
-              <div className="col-lg-12">
-                <div className="project-item">
-                  <div className="project-info">
-                    <img src={lettuceEat} alt="no internet connection"></img>
-                  </div>
-                </div>
-                <div className="project-name">
-                  <a href="https://www.google.com"> Lettuce Eat </a>
-                </div>
-              </div>
-            </OwlCarousel>
-          </div>
-        </div>
-      </section>
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block" src={weather} alt="Third slide" />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   );
 }
